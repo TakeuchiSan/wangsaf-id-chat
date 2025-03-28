c2ad7ef8e0602482831b65f2f309977d65e65df9
PiuPiu <141508472+TakeuchiSan@users.noreply.github.com>
2025-03-28 12:48:49 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold">WANGSAF ID Chat</h1>
+    </div>
+  );
+}
